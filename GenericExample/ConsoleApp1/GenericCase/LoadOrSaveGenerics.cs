dcf590569082c8144af3b1867d9b7d3946b795db
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2023-05-01 17:10:53 +0100

    Generic product and person added to excel (without config file).

---
+++ GenericExample/ConsoleApp1/GenericCase/LoadOrSaveGenerics.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.GenericCase
+{
+    public class LoadOrSaveGenerics
+    {
+        public List<T> loadData<T>(string path) where T : class, new() {
+            var lines = System.IO.File.ReadAllLines(path).ToList();
+            List<T> result = new List<T>();
+            var entry = new T();
+            var cols = entry.GetType().GetProperties();
+            var headers = lines[0].Split(',');
+            lines.RemoveAt(0);
+             
+            foreach ( var line in lines )
+            {
+                entry = new T();
+                var value = line.Split(",");
+                foreach (var header in headers.Select((value, id) => new { id, value }))
+                {
+                    foreach (var col in cols)
+                    {
+                        if(col.Name == header.value)
+                        {
+                            col.SetValue(entry, Convert.ChangeType(value[header.id], col.PropertyType));
+                        }
+                    }
+                }
+                result.Add(entry);
+            } 
+            return result;
+        }
+
+        public List<T> saveData<T>(List<T> data, string path) where T : class, new()
+        {
+            var lines = new List<string>();
+            List<T> result = new List<T>();
+            StringBuilder line = new StringBuilder();
+            var cols = data[0].GetType().GetProperties();
+            //lines.RemoveAt(0);
+
+            foreach (var col in cols)
+            {
+                line.Append(col.Name);
+                line.Append(',');
+            }
+            lines.Add(line.ToString().Substring(0, line.Length - 1));
+
+            foreach(var item in data)
+            {
+                line = new StringBuilder();
+                foreach(var col in cols)
+                {
+                    line.Append(col.GetValue(item));
+                    line.Append(",");
+                }
+                lines.Add(line.ToString().Substring(0, line.Length - 1));
+            }
+            System.IO.File.WriteAllLines(path, lines.ToArray());
+            return result;
+        }
+    }
+}
---
+++ GenericExample/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Generics.GenericCase;
+using Generics.Models;
+
+namespace Generics { 
+public class Program{
+
+		public static void Main()
+		{
+			RunApp();
+		}
+        public static void RunApp()
+		{
+            //save person in excel
+			var people = new List<Person>();
+			PopulatePeople(people);
+
+			var path = @"C:\Users\aabha\person.csv";
+			LoadOrSaveGenerics generic = new LoadOrSaveGenerics(); 
+			generic.saveData(people, path); //generic method
+
+
+            //save product in excel
+            var product = new List<Product>();
+            PopulateProduct(product);
+
+            var pathProd = @"C:\Users\aabha\product.csv";
+            generic.saveData(product, pathProd); //generoic method
+
+        }
+		private static void PopulatePeople(List<Person> people)
+		{
+			var person = new Person() { FirstName = "Aabhaas-1", LastName = "Ma", Id = 1 };
+			people.Add(person);
+            var personTwo = new Person() { FirstName = "Shivangi", LastName = "Mu", Id = 2 };
+            people.Add(personTwo);
+        }
+
+        private static void PopulateProduct(List<Product> products)
+        {
+            var product = new Product() { Name = "Bulb", Description = "Light bulb", Id = 1 };
+            products.Add(product);
+            var productTwo = new Product() { Name = "Baloon", Description = "Light baloon", Id = 2 };
+            products.Add(productTwo);
+        }
+    }
+}
